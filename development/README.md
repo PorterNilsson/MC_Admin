9437ec1595c58165b7ba57e346d198c5e04843ad
PorterNilsson <porter.nilsson@gmail.com>
2025-06-16 23:27:11 -0600

    Updated README and userspace script

--- development/README.md
+++ development/README.md
@@ -1,3 +1,5 @@
 # Development Environment Setup Instructions
 
 - To begin, cd to the development directory and run `./build_dev_env.sh`
+- VirtualBox sometimes has issues creating a VM from the command line when the GUI is open, so ensure you have quit the VirtualBox GUI before running the script
+- To wipe the userspace environment and start fresh, just run `set_up_userspace.sh`. This will be run automatically during a full setup.
--- development/set_up_userspace.sh
+++ development/set_up_userspace.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
+set -e
 
-sudo apt-get update
-sudo apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
 # Install Docker
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo docker run hello-world
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker run hello-world
 
-sudo groupadd docker
+groupadd docker
 
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+systemctl enable docker.service
+systemctl enable containerd.service
 
 # Install Golang
 wget https://go.dev/dl/go1.24.3.linux-arm64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.3.linux-arm64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
+echo 'export PATH=$PATH:/usr/local/go/bin' | tee -a /etc/profile
 . /etc/profile
 go version
-rm -f go1.24.3.linux-amd64.tar.gz
+rm -f go1.24.3.linux-arm64.tar.gz
 
 # Create project folder
 mkdir MC_Admin
